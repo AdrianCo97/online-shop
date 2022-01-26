c07949a397774570a9f89ebf1065e5767f8a7d4b
AdrianCo97 <adrian.corazza@outlook.com>
2022-01-26 10:26:19 +0100

    Created a navbar with mui

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
+import { AppBar, Box, Toolbar, Button } from "@mui/material";
 import "./navbar.css";
 
 function Navbar() {
   return (
-    <div class="background">
-      <nav>
-        <li>Herr</li>
-        <li>Dam</li>
-      </nav>
-    </div>
+    <Box>
+      <AppBar position="static" color="secondary">
+        <Toolbar sx={{ justifyContent: 'space-between'}}>
+          <Box sx={{ justifyContent: 'space-evenly' }}>
+            <Button size="large" color="inherit">Male</Button>
+            <Button size="large" color="inherit">Female</Button>
+            <Button size="large" color="inherit">Children</Button> 
+          </Box>
+          <Button size="large" color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
