32581a71b67854b2c3dcd7faf3005e6e7d811a9f
AdrianCo97 <adrian.corazza@outlook.com>
2022-01-26 12:45:30 +0100

    Created a lot of styling for the site.

--- src/components/Main/App.js
+++ src/components/Main/App.js
@@ -4,10 +4,13 @@ import "./app.css";
 
 function App() {
 
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => setProducts(data));
+    console.log(products);
   }, [])
 
   return (
@@ -15,6 +18,19 @@ function App() {
       <Navbar />
       <div class="main-content">
         <h3>What's popular</h3>
+        <div class="products">
+        {
+          products.map(product => {
+            return (
+              <div class="product" key={product.id}>
+                  <img src={product.image}></img>
+                  <p class="title">{product.title}</p>
+                  <p>{product.price} SEK</p>
+              </div>     
+            )
+          })
+        }
+        </div>
       </div>
     </body>
     
