8ad4e040c2fafd877bbfce9d7c4625f010c6d3b2
AdrianCo97 <adrian.corazza@outlook.com>
2022-01-28 14:40:25 +0100

    Created an async await funtion for getting the data from the api.

--- src/components/Main/App.js
+++ src/components/Main/App.js
@@ -7,9 +7,17 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then(response => response.json())
-      .then(data => setProducts(data));
+    const getData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        console.log(data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
   }, [])
 
   return (
