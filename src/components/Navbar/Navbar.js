d39c879d3520ecab8763d6c623907f50c79cbd9a
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-05-28 14:36:38 +0200

    Created links to different pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,27 @@
 import "./navbar.css";
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-
+import { Link } from "react-router-dom";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Clothes from "../Categories/Clothes/Clothes.js";
+import Electronics from "../Categories/Electronics/Electronics.js";
 function Navbar() {
   return (
     <div className="background">
       <div className="clothing-category">
-        <li>Electronics</li>
-        <li>Clothes</li>
+        <Link to="/electronics">
+          <li>Electronics</li>
+        </Link>
+        <Link to="/clothes">
+          <li>Clothes</li>
+        </Link>
         <li>Accessories</li>
       </div>
 
       <div className="account-category">
         <li>Login</li>
-        <ShoppingCartOutlinedIcon fontSize="medium"/>
+        <ShoppingCartOutlinedIcon fontSize="medium" />
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar;
