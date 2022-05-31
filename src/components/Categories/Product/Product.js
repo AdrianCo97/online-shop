1aee1aaaf3c0b88b493bf431dcea658bd81d508c
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-05-31 11:20:34 +0200

    Changed the layout a bit

--- src/components/Categories/Product/Product.js
+++ src/components/Categories/Product/Product.js
@@ -1,28 +1,36 @@
 import { useEffect, useState } from "react";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Navbar from "../../Navbar/Navbar.js";
 import "./product.css";
 
 function Product() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [product, setProduct] = useState([]);
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${id}`).then((response) => {
-      return response.json();
-    }).then((data) => {
-      setProduct(data);
-    })
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProduct(data);
+      });
   }, [id]);
 
   return (
     <div>
       <Navbar />
       <div className="pageContent">
-        <img src={product.image}></img>
-        <p>{product.title}</p>
-        <p>{product.description}</p>
-        <p>{product.price} €</p> 
+        <div className="header">
+          <img src={product.image}></img>
+          <p>{product.title}</p>
+          <h1 className="price">{product.price} €</h1>
+          <button>Add to cart</button>
+        </div>
+
+        <div className="productInfo">
+          <p>{product.description}</p>
+        </div>
       </div>
     </div>
   );
