987630932b2ab3a3afd3c8e685fd4f0668369130
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-05-31 10:26:39 +0200

    Created an onclick funtion for the product

--- src/components/Categories/Logic/RenderArrayContent.js
+++ src/components/Categories/Logic/RenderArrayContent.js
@@ -1,16 +1,25 @@
+import Product from "../Product/Product.js";
+import { useNavigate } from "react-router-dom";
+
 function RenderSpecificClothes({ array }) {
+
+  const navigate = useNavigate();
+  const handleClick = (id) => {
+    navigate(`/product/${id}`);
+  }
+
   return (
     <div className="products">
       {array.map((array) => {
-        return(
-          <div className="product" key={array.id}>
-          <img src={array.image}></img>
-          <div className="product-info">
-            <p className="title">{array.title}</p>
-            <p className="price">{array.price} SEK</p>
+        return (
+          <div className="product" key={array.id} onClick={() => handleClick(array.id)}>
+            <img src={array.image}></img>
+            <div className="product-info">
+              <p className="title">{array.title}</p>
+              <p className="price">{array.price} SEK</p>
+            </div>
           </div>
-        </div>
-        )
+        );
       })}
     </div>
   );
