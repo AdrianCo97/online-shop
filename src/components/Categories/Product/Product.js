5257892eccf20ba9c07820e4069ba1cf25e32e4f
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-09 13:33:58 +0200

    Created a function for the addtocart button

--- src/components/Categories/Product/Product.js
+++ src/components/Categories/Product/Product.js
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../../Navbar/Navbar.js";
 import "./product.css";
+import {CartContext} from "../../../contexts/CartContext.js";
 const jsonData = require("../../../data/products.json");
-
 function Product() {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
+  const {productsInCart, setProductsInCart} = useContext(CartContext);
+
 
   useEffect(() => {
     for(let i = 0; i < jsonData.length; i++) {
@@ -16,16 +18,22 @@ function Product() {
     }
   }, [id]);
 
+  const addToCartFunction = () => {
+    setProductsInCart(productsInCart + 1);
+  }
+  
+
   return (
     <div>
       <Navbar />
       <div className="pageContent">
         <div className="header">
+          <p>Products in cart: {productsInCart}</p>
           <img src={product.image}></img>
           <p>{product.title}</p>
           <div className="footer">
             <h1 className="price">{product.price} €</h1>
-            <button>Add to cart</button>
+            <button onClick={addToCartFunction}>Add to cart</button>
           </div>
         </div>
 
