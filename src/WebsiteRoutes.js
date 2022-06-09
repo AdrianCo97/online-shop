5a5454f34d5ebb3f9276867f1ffca22049e2c774
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-09 13:33:03 +0200

    Setup routes for a global context

--- src/WebsiteRoutes.js
+++ src/WebsiteRoutes.js
@@ -1,11 +1,11 @@
-import { React, useState, Fragment } from "react";
+import { React, useState} from "react";
 import Home from "./components/Categories/Home/Home.js";
 import Clothes from "./components/Categories/Clothes/Clothes.js";
 import Electronics from "./components/Categories/Electronics/Electronics.js";
 import Accessories from "./components/Categories/Accessories/Accessories.js";
 import Product from "./components/Categories/Product/Product.js";
-import CartContext from "./contexts/CartContext.js";
-import NavBar from "./components/Navbar/Navbar.js";
+import {CartContext} from "./contexts/CartContext.js"
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function WebsiteRoutes() {
   const [productsInCart, setProductsInCart] = useState(0);
@@ -13,11 +13,11 @@ function WebsiteRoutes() {
     <CartContext.Provider value={{ productsInCart, setProductsInCart }}>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home values={{productsInCart, setProductsInCart}}/>} />
-          <Route path="/clothes" element={<Clothes values={{productsInCart, setProductsInCart}} />} />
-          <Route path="/electronics" element={<Electronics values={{productsInCart, setProductsInCart}} />} />
-          <Route path="/accessories" element={<Accessories values={{productsInCart, setProductsInCart}} />} />
-          <Route path="/product/:id" element={<Product values={productsInCart} />} />
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/clothes" element={<Clothes />} />
+          <Route path="/electronics" element={<Electronics/>} />
+          <Route path="/accessories" element={<Accessories/>} />
+          <Route path="/product/:id" element={<Product/>} />
         </Routes>
       </BrowserRouter>
     </CartContext.Provider>
