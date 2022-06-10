95373d88c0e08b55f32c850b4ca5c5091fd18792
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-10 10:16:56 +0200

    The user can now add products to the cart

--- src/components/Categories/Product/Product.js
+++ src/components/Categories/Product/Product.js
@@ -2,38 +2,38 @@ import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../../Navbar/Navbar.js";
 import "./product.css";
-import {CartContext} from "../../../contexts/CartContext.js";
+import { CartContext } from "../../../contexts/CartContext.js";
 const jsonData = require("../../../data/products.json");
 function Product() {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
-  const {productsInCart, setProductsInCart} = useContext(CartContext);
-
+  const { productsInCart, setProductsInCart } = useContext(CartContext);
 
   useEffect(() => {
-    for(let i = 0; i < jsonData.length; i++) {
-      if(jsonData[i].id == id){
+    for (let i = 0; i < jsonData.length; i++) {
+      if (jsonData[i].id == id) {
         setProduct(jsonData[i]);
-      } 
+      }
     }
   }, [id]);
 
-  const addToCartFunction = () => {
-    setProductsInCart(productsInCart + 1);
-  }
-  
+  const addToCartFunction = (product) => {
+    setProductsInCart([...productsInCart, product]);
+  };
 
   return (
     <div>
       <Navbar />
       <div className="pageContent">
         <div className="header">
-          <p>Products in cart: {productsInCart}</p>
           <img src={product.image}></img>
           <p>{product.title}</p>
           <div className="footer">
             <h1 className="price">{product.price} €</h1>
-            <button onClick={addToCartFunction}>Add to cart</button>
+
+            <button onClick={() => addToCartFunction(product)}>
+              Add to cart
+            </button>
           </div>
         </div>
 
