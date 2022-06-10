3b178e2490612ff0e8f7242a180c7494700b30af
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-10 10:50:01 +0200

    Setup the shoppingcart page

---
+++ src/components/Categories/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from "react";
+import {CartContext} from "../../../contexts/CartContext.js";
+
+function ShoppingCart(){
+
+    const {productsInCart, setProductsInCart} = useContext(CartContext);
+
+    return(
+        {
+            
+        }
+    )
+}
+
+export default ShoppingCart;
