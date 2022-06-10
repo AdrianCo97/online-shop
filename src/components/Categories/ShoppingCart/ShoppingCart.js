b2336d8dffd59d671b55ba03d94857f260525912
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-10 12:56:12 +0200

    Looped out some basic info about the products

--- src/components/Categories/ShoppingCart/ShoppingCart.js
+++ src/components/Categories/ShoppingCart/ShoppingCart.js
@@ -1,15 +1,26 @@
-import {useState, useEffect} from "react";
-import {CartContext} from "../../../contexts/CartContext.js";
+import { useState, useContext } from "react";
+import "../ShoppingCart/ShoppingCart.css";
+import { CartContext } from "../../../contexts/CartContext.js";
+import Navbar from "../../Navbar/Navbar.js";
 
-function ShoppingCart(){
+function ShoppingCart() {
+  const { productsInCart, setProductsInCart } = useContext(CartContext);
 
-    const {productsInCart, setProductsInCart} = useContext(CartContext);
+  return (
+    <div>
+      <Navbar />
+      {productsInCart.map((product) => {
+        return (
+          <div className="productinCart" key={product.id}>
+            <p>{product.title}</p>
+            <p>{product.price} €</p>
+          </div>
+        );
+      })}
 
-    return(
-        {
-            
-        }
-    )
+      <button>Go to checkout</button>
+    </div>
+  );
 }
 
 export default ShoppingCart;
