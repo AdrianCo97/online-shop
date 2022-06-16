f5c2b567aec7f21282713b144eb239795dafd9c8
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-16 12:44:31 +0200

    Created the checkout component

---
+++ src/components/Categories/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import Navbar from "../../Navbar/Navbar";
+
+function Checkout() {
+  return (
+    <div>
+      <Navbar />
+      <div>Checkout page</div>
+    </div>
+  );
+}
+
+export default Checkout;
