097ae150c1732490755519dc5f6e8c5a0b6cc06a
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-22 16:25:51 +0200

    Added input fields

--- src/components/Categories/Checkout/Checkout.js
+++ src/components/Categories/Checkout/Checkout.js
@@ -1,10 +1,18 @@
 import Navbar from "../../Navbar/Navbar";
+import TextField from '@mui/material/TextField';
 
 function Checkout() {
   return (
     <div>
       <Navbar />
-      <div>Checkout page</div>
+      <form>
+        <TextField label="First Name" variant="outlined" />
+        <TextField label="Last Name" variant="outlined" />
+        <TextField label="Email" variant="outlined" />
+        <TextField label="Phone" variant="outlined" />
+        <TextField label="Address" variant="outlined" />
+        <TextField label="City" variant="outlined" />
+      </form>
     </div>
   );
 }
