e658c4642272890b1e0f64543f8261f79e00ccd9
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-06-22 16:17:41 +0200

    Added another route for the checkout page

--- src/WebsiteRoutes.js
+++ src/WebsiteRoutes.js
@@ -1,11 +1,12 @@
-import { React, useState} from "react";
+import { React, useState } from "react";
 import Home from "./components/Categories/Home/Home.js";
 import Clothes from "./components/Categories/Clothes/Clothes.js";
 import Electronics from "./components/Categories/Electronics/Electronics.js";
 import Accessories from "./components/Categories/Accessories/Accessories.js";
 import Product from "./components/Categories/Product/Product.js";
 import ShoppingCart from "./components/Categories/ShoppingCart/ShoppingCart.js";
-import {CartContext} from "./contexts/CartContext.js"
+import Checkout from "./components/Categories/Checkout/Checkout.js";
+import { CartContext } from "./contexts/CartContext.js";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function WebsiteRoutes() {
@@ -14,12 +15,13 @@ function WebsiteRoutes() {
     <CartContext.Provider value={{ productsInCart, setProductsInCart }}>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/clothes" element={<Clothes />} />
-          <Route path="/electronics" element={<Electronics/>} />
-          <Route path="/accessories" element={<Accessories/>} />
-          <Route path="/product/:id" element={<Product/>} />
-          <Route path="/shoppingcart" element={<ShoppingCart/>} />
+          <Route path="/electronics" element={<Electronics />} />
+          <Route path="/accessories" element={<Accessories />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/shoppingcart" element={<ShoppingCart />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </BrowserRouter>
     </CartContext.Provider>
