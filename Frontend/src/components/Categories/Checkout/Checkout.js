855df19e4894393615cbebb693efccead9261855
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-03 16:07:17 +0200

    Moved the files to a frontend map

---
+++ Frontend/src/components/Categories/Checkout/Checkout.js
@@ -0,0 +1,72 @@
+import Navbar from "../../Navbar/Navbar";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import FormHelperText from "@mui/material/FormHelperText";
+
+function Checkout() {
+  return (
+    <div>
+      <Navbar />
+
+      <Grid container spacing={3}>
+        <Grid item>
+          <TextField
+            size="small"
+            fullWidth
+            label="First Name"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
+        <Grid item >
+          <TextField
+            size="small"
+            fullWidth
+            label="Last Name"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            size="small"
+            fullWidth
+            label="Email"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            size="small"
+            fullWidth
+            label="Phone"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            size="small"
+            fullWidth
+            label="Address"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            size="small"
+            fullWidth
+            label="City"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Checkout;
