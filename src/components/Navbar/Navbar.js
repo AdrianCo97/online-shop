8f5ef438c5757e6432c26e59538d6f5c55c18387
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-03 15:53:02 +0200

    Added a link to the account page.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import "./navbar.css";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PersonIcon from "@mui/icons-material/Person";
 import { CartContext } from "../../contexts/CartContext.js";
 import Badge from "@mui/material/Badge";
 
@@ -11,7 +12,7 @@ function Navbar() {
   const navigate = useNavigate();
   const navigateToShoppingCart = () => {
     navigate("/shoppingcart");
-  }
+  };
 
   return (
     <div className="background">
@@ -28,9 +29,19 @@ function Navbar() {
       </div>
 
       <div className="account-category">
+        <div className="account-section">
+          <Link className="link" to="/account">
+            <PersonIcon sx={{ verticalAlign: "middle" }} />
+            Login
+          </Link>
+        </div>
         <div className="cartSection">
           <Badge badgeContent={productsInCart.length} color="success">
-            <ShoppingCartIcon className="shoppingCart" fontSize="medium" onClick={() => navigateToShoppingCart()} />
+            <ShoppingCartIcon
+              className="shoppingCart"
+              fontSize="medium"
+              onClick={() => navigateToShoppingCart()}
+            />
           </Badge>
         </div>
       </div>
