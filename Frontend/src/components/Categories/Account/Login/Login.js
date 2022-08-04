6f7698e9e0a9a8a1763aeb3c7d99ac41d90b4ef3
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-04 15:33:05 +0200

    Setup some basic HTML for the login page

--- Frontend/src/components/Categories/Account/Login/Login.js
+++ Frontend/src/components/Categories/Account/Login/Login.js
@@ -1,11 +1,25 @@
-import "./login.css"
+import { TextField } from "@mui/material";
+import "./login.css";
 
-function Login(){
-    return(
-        <div className="login-box">
-            <p>Login</p>
+function Login() {
+  return (
+    <div className="login-box">
+      <p>Login</p>
+      <div className="body">
+        <TextField id="email" label="Email" variant="outlined" sx={{ mb: 2 }} />
+        <TextField
+          id="password"
+          label="Password"
+          variant="outlined"
+          sx={{ mb: 2 }}
+        />
+        <div className="buttons">
+          <button className="submit-button">Login</button>
+          <button className="submit-button">Not registered?</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Login;
