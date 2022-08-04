8545447e2821cdf42af92e352c46a19738a3c62e
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-04 16:23:15 +0200

    Made it so that you can't autocomple passwords

--- Frontend/src/components/Categories/Account/Login/Login.js
+++ Frontend/src/components/Categories/Account/Login/Login.js
@@ -1,30 +1,41 @@
 import { TextField } from "@mui/material";
-import {useState} from "React";
+import { useState } from "react";
 import "./login.css";
 
 function Login() {
+  const [passwordState, setPasswordState] = useState();
 
-    const[passwordState, setPasswordState] = useState({
-        password: "",
-        showPasswords: false
-    })
+  const handleChange = (event) => {
+    setPasswordState(event.target.value);
+  };
+
   return (
-    <div className="login-box">
+    <form className="login-box">
       <p>Login</p>
       <div className="body">
-        <TextField id="email" label="Email" variant="outlined" sx={{ mb: 2 }} />
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          type="email"
+          sx={{ mb: 2 }}
+        />
         <TextField
           id="password"
           label="Password"
           variant="outlined"
+          type="password"
+          autoComplete="off"
+          onChange={(event) => handleChange(event)}
           sx={{ mb: 2 }}
+          
         />
         <div className="buttons">
           <button className="submit-button">Login</button>
           <button className="submit-button">Not registered?</button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
