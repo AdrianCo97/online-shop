8e443aaba54edd534b13fb6df822d9dbde6e9d75
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-07 14:20:41 +0200

    Created some basic html

--- Frontend/src/components/Categories/Account/CreateAccount/CreateAccount.js
+++ Frontend/src/components/Categories/Account/CreateAccount/CreateAccount.js
@@ -1,10 +1,22 @@
+import { TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import "./createAccount.css";
+
 
 function CreateAccount() {
     return (
-        <div className="create-account-box">
+        <form className="create-account-box">
             <p>Create Account</p> 
+            <div className="header">
+      <h2>Login</h2>
+      </div>
+      <div className="body">
+        <div className="buttons">
+          <button className="submit-button">Login</button>
+          <button>Already have an account?</button>
         </div>
+      </div>
+        </form>
     )
 }
 
