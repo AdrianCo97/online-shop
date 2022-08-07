c57540545c232759f159a41ee876cda1cc63c66c
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-07 14:48:04 +0200

    Implemented input fields

--- Frontend/src/components/Categories/Account/CreateAccount/CreateAccount.js
+++ Frontend/src/components/Categories/Account/CreateAccount/CreateAccount.js
@@ -6,9 +6,13 @@ function CreateAccount() {
   return (
     <form className="create-account-box">
       <div className="header">
-        <p>Create Account</p>
+        <h2>Create Account</h2>
       </div>
       <div className="body">
+        <TextField label="firstname" sx={{ mb: 2 }} />
+        <TextField label="lastname" sx={{ mb: 2 }} />
+        <TextField label="email" sx={{ mb: 2 }} />
+        <TextField label="password" type="password" sx={{ mb: 2 }} />
         <div className="buttons">
           <button>Create account</button>
           <button>Already have an account?</button>
