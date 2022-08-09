0bd656216403f346defb4958793772203733bacd
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-09 17:15:37 +0200

    Made it so that passwords get encrypted

--- Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
+++ Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.onlineshopAPI.controller;
 
 import com.example.onlineshopAPI.doa.UserRepository;
 import com.example.onlineshopAPI.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -12,8 +15,11 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserRepository userRepository;
-    public UserController(UserRepository userRepository){
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
@@ -22,6 +28,7 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User can't be null");
         }
         else{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
             return new ResponseEntity<>("Account created", HttpStatus.OK);
         }
