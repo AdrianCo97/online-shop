ff35c542ba986ec3ac0fc7377433e1ef31646917
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-09 13:58:18 +0200

    Added a missing annotation

--- Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
+++ Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@RestController
 public class UserController {
 
     private final UserRepository userRepository;
