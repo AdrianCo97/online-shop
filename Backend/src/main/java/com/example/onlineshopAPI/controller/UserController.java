7b398040ba3de25faf32dc0af2500847f10e2152
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-09 14:01:13 +0200

    Made it so that the postmapping returns a response

--- Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
+++ Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.onlineshopAPI.controller;
 import com.example.onlineshopAPI.doa.UserRepository;
 import com.example.onlineshopAPI.model.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +19,13 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<String> createUser(@RequestBody User user){
         if(user == null){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User can't be null");
         }
         else{
             userRepository.save(user);
+            return new ResponseEntity<>("Account created", HttpStatus.OK);
         }
     }
 
