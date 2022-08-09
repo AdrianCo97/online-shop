c8cb846bc8e735ca14e11fe92049c75c833b062e
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-09 15:33:05 +0200

    Made it so that you have to input values

--- Frontend/src/components/Categories/Account/CreateAccount/CreateAccount.js
+++ Frontend/src/components/Categories/Account/CreateAccount/CreateAccount.js
@@ -48,28 +48,28 @@ function CreateAccount() {
         <h2>Create Account</h2>
       </div>
       <div className="body">
-        <TextField
+        <TextField required
           label="firstname"
           sx={{ mb: 2 }}
           onChange={(e) =>
             setUserInput({ ...userInput, firstname: e.target.value })
           }
         />
-        <TextField
+        <TextField required
           label="lastname"
           sx={{ mb: 2 }}
           onChange={(e) =>
             setUserInput({ ...userInput, lastname: e.target.value })
           }
         />
-        <TextField
+        <TextField required
           label="email"
           sx={{ mb: 2 }}
           onChange={(e) =>
             setUserInput({ ...userInput, email: e.target.value })
           }
         />
-        <TextField
+        <TextField required
           label="password"
           type={passwordState.showPassword ? "text" : "password"}
           sx={{ mb: 2 }}
