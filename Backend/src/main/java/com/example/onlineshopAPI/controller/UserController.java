5b9357b6544ba2dc88649c2ccad1d8fc54ca9f15
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-16 12:55:36 +0200

    Moved logic from controller to the service class

--- Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
+++ Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.onlineshopAPI.controller;
 
 import com.example.onlineshopAPI.doa.UserRepository;
 import com.example.onlineshopAPI.model.User;
+import com.example.onlineshopAPI.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,60 +17,32 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder){
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/users")
     public ResponseEntity<String> createUser(@RequestBody User user){
-        if(user == null){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User can't be null");
-        }
-        else{
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            userRepository.save(user);
-            return new ResponseEntity<>("Account created", HttpStatus.OK);
-        }
+        return userService.createUser(user);
     }
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable Long id) {
-        User user = userRepository.findById(id).
-                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
-        return user;
+        return userService.getUser(id);
     }
 
     @GetMapping("/users")
     public Iterable<User> getUsers(){
-        if(((List<User>)userRepository.findAll()).size() == 0){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This database contains no users");
-        }
-        else{
-            return userRepository.findAll();
-        }
+        return userService.getUsers();
     }
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist."));
-        userRepository.delete(user);
-        return new ResponseEntity<>("User deleted", HttpStatus.OK);
+        return userService.deleteUser(id);
     }
 
     @PutMapping("/users/{id}")
     public ResponseEntity<User> changeUser(@PathVariable Long id, @RequestBody User user){
-        User userToChange = userRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist"));
-        userToChange.setFirstname(user.getFirstname());
-        userToChange.setLastname(user.getLastname());
-        userToChange.setEmail(user.getEmail());
-        userToChange.setPassword(user.getPassword());
-        return ResponseEntity.ok(userToChange);
+        return userService.changeUser(id, user);
     }
 
 
--- Backend/src/main/java/com/example/onlineshopAPI/service/UserService.java
+++ Backend/src/main/java/com/example/onlineshopAPI/service/UserService.java
@@ -1,34 +1,69 @@
 package com.example.onlineshopAPI.service;
 import com.example.onlineshopAPI.doa.UserRepository;
 import com.example.onlineshopAPI.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class UserService implements UserServiceInterface{
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public ResponseEntity<String> createUser(User user) {
+        if(user == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User can't be null");
+        }
+        else{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+            return new ResponseEntity<>("Account created", HttpStatus.OK);
+        }
     }
 
     @Override
-    public User saveUser(User user) {
-        return null;
+    public ResponseEntity<String> deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist."));
+        userRepository.delete(user);
+        return new ResponseEntity<>("User deleted", HttpStatus.OK);
     }
 
     @Override
-    public void deleteUser(User user) {
+    public User getUser(Long id) {
+        User user = userRepository.findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        return user;
+    }
 
+    public Iterable<User> getUsers(){
+        if(((List<User>)userRepository.findAll()).size() == 0){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This database contains no users");
+        }
+        else{
+            return userRepository.findAll();
+        }
     }
 
     @Override
-    public User getUserById(Long Id) {
-        return null;
-    }
-
-    @Override
-    public void changeUser(User user) {
-
+    public ResponseEntity<User> changeUser(Long id, User user) {
+        User userToChange = userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist"));
+        userToChange.setFirstname(user.getFirstname());
+        userToChange.setLastname(user.getLastname());
+        userToChange.setEmail(user.getEmail());
+        userToChange.setPassword(user.getPassword());
+        return ResponseEntity.ok(userToChange);
     }
 }
