a7e6689d708fbd5948012b66fb2df3598b448e20
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-16 15:15:19 +0200

    Injected the userservice bean

--- Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
+++ Backend/src/main/java/com/example/onlineshopAPI/controller/UserController.java
@@ -1,24 +1,20 @@
 package com.example.onlineshopAPI.controller;
 
-import com.example.onlineshopAPI.doa.UserRepository;
 import com.example.onlineshopAPI.model.User;
 import com.example.onlineshopAPI.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 public class UserController {
 
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/users")
     public ResponseEntity<String> createUser(@RequestBody User user){
