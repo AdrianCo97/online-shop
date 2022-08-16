a8051d75eb46ae8551f487cede5f05ef32e0d8a9
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-16 12:55:56 +0200

    Added an annotation

--- Backend/src/main/java/com/example/onlineshopAPI/doa/UserRepository.java
+++ Backend/src/main/java/com/example/onlineshopAPI/doa/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.onlineshopAPI.doa;
 import com.example.onlineshopAPI.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
