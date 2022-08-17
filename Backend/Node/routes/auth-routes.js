13f960599fd77cb413e39b33bf7bbbb5dd254582
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-17 17:07:36 +0200

    Created a login route

---
+++ Backend/Node/routes/auth-routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+    const username = req.params.username;
+    const password = req.params.password;
+
+    
+})
