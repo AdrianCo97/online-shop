571b00281e3a90efc800283d5d7e16c04e91f698
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-18 15:51:19 +0200

    Created routing for the cart routes

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth-routes.js";
+import cartRoutes from "./routes/cart-routes.js";
 
 const app = express();
 const port = 5000;
@@ -10,7 +11,9 @@ dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri).then(console.log("Connected"));
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(port, () => console.log("listening on port " + port));
