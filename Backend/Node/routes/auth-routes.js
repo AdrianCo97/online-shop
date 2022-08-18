ba940803953523d65b89396ecde725f8fbb50ca2
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-18 17:58:15 +0200

    Made it so that passwords get encrypted

--- Backend/Node/routes/auth-routes.js
+++ Backend/Node/routes/auth-routes.js
@@ -1,29 +1,37 @@
-import express from 'express';
+import express from "express";
 import User from "../models/User.js";
+import { encryption } from "../encryption.js";
 
 const router = express.Router();
 
-router.post("/register", async(req, res) => {
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const email = req.body.email;
-    const password = req.body.password;  
+router.post("/register", async (req, res) => {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const password = await encryption(req.body.password);
 
-    try{
-        const newUser = new User({
-            firstname: firstname,
-            lastname: lastname,
-            email: email,
-            password: password
-        })
+  const foundUser = await User.findOne({ email: email });
 
-        const savedUser = await newUser.save();
+  if (foundUser) {
+    res.status(400).json({ error: "A user with that email already exists." });
+  } else {
+    try {
+      const newUser = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+      });
 
-        res.status(200).json(savedUser);
+      const savedUser = await newUser.save();
+
+      res.status(200).json(savedUser);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong with the registration." });
     }
-    catch(err){
-        res.status(500).json(err.message);
-    }
-})
+  }
+});
 
 export default router;
