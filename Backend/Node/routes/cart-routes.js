fbf00105ff84a9ea3bd535840fd985028b54a5d5
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-18 15:50:15 +0200

    Created a post for saving a cart

---
+++ Backend/Node/routes/cart-routes.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import ShoppingCart from "../models/ShoppingCart.js";
+
+const router = express.Router();
+
+router.post("/savecart", async(req, res) => {
+    const email = req.body.email;
+    const cartTitle = req.body.cartTitle;
+    const products = req.body.products;
+    try{
+        const newShoppingCart = new ShoppingCart({
+            email: email,
+            cartTitle: cartTitle,
+            products: products
+        })
+
+        const savedShoppingCart = await newShoppingCart.save();
+
+        res.status(200).json(savedShoppingCart)
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+})
+
+export default router;
