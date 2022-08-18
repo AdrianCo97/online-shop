30a43fa216e0048c1687d558b73146dbdaa31a1c
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-18 14:01:47 +0200

    Created the connection to the database

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -1,24 +1,16 @@
 import mongoose from "mongoose";
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth-routes.js";
 
 const app = express();
 const port = 5000;
 
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+dotenv.config();
 
-mongoose.connect(
-  `mongodb+srv://${username}:${password}@online-shop-cluster.qaxjrbx.mongodb.net/?retryWrites=true&w=majority`,
-  () => {
-    try {
-      console.log("Connected to database.");
-    } catch (e) {
-      console.log(e.message);
-    }
-  }
-);
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri).then(console.log("Connected"));
 
-app.user("/api/auth/", authRoutes);
+app.use("/api/auth/", authRoutes);
 
 app.listen(port, () => console.log("listening on port " + port));
