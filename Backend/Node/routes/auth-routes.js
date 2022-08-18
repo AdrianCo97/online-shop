a5b6f0258ac5f7785702b7215c9326240c18be01
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-18 14:01:31 +0200

    Installed the dotenv package

--- Backend/Node/routes/auth-routes.js
+++ Backend/Node/routes/auth-routes.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import User from "../models/User.js";
 
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-    const username = req.params.username;
+router.post("/register", async(req, res) => {
+    const firstname = req.params.firstname;
+    const lastname = req.params.lastname;
+    const email = req.params.email;
     const password = req.params.password;  
+
+    try{
+        const newUser = new User({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: password
+        })
+
+        const savedUser = await newUser.save();
+
+        res.status(200).json(savedUser);
+    }
+    catch(err){
+        res.status(500).json(err.message);
+    }
 })
 
 export default router;
