68737590c419d507cc83d4e1b7c345c5eade03e0
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-18 12:27:15 +0200

    Connected the api to the mongodb database

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -1,6 +1,21 @@
+import mongoose from "mongoose";
+import User from "./models/User.js";
 import express from "express";
 
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@online-shop-cluster.qaxjrbx.mongodb.net/?retryWrites=true&w=majority`,
+  () => {
+    try {
+      console.log("Connected to database.");
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+);
 const app = express();
 const port = 5000;
 
-app.listen(port, () => console.log('listening on port ' + port));
+app.listen(port, () => console.log("listening on port " + port));
