d8fafb9c897397bd8aba6ca8f0016d53605391f5
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-19 15:53:48 +0200

    Created a usercontext

--- Frontend/src/WebsiteRoutes.js
+++ Frontend/src/WebsiteRoutes.js
@@ -7,29 +7,32 @@ import Product from "./components/Categories/Product/Product.js";
 import ShoppingCart from "./components/Categories/ShoppingCart/ShoppingCart.js";
 import Checkout from "./components/Categories/Checkout/Checkout.js";
 import Login from "./components/Categories/Account/Login/Login.js";
-import CreateAccount from "./components/Categories/Account/CreateAccount/CreateAccount.js"
+import CreateAccount from "./components/Categories/Account/CreateAccount/CreateAccount.js";
 import { CartContext } from "./contexts/CartContext.js";
+import { UserContext } from "./contexts/UserContext.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 function WebsiteRoutes() {
   const [productsInCart, setProductsInCart] = useState([]);
+  const [user, setUser] = useState({ firstname: "", accesstoken: "" });
   return (
-    <CartContext.Provider value={{ productsInCart, setProductsInCart }}>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/clothes" element={<Clothes />} />
-          <Route path="/electronics" element={<Electronics />} />
-          <Route path="/accessories" element={<Accessories />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/shoppingcart" element={<ShoppingCart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/createaccount" element={<CreateAccount />} />
-        </Routes>
-      </BrowserRouter>
-    </CartContext.Provider>
+    <UserContext.Provider value={{ user, setUser }}>
+      <CartContext.Provider value={{ productsInCart, setProductsInCart }}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/clothes" element={<Clothes />} />
+            <Route path="/electronics" element={<Electronics />} />
+            <Route path="/accessories" element={<Accessories />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/shoppingcart" element={<ShoppingCart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/createaccount" element={<CreateAccount />} />
+          </Routes>
+        </BrowserRouter>
+      </CartContext.Provider>
+    </UserContext.Provider>
   );
 }
 
