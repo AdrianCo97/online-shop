d58335ceb355bc5959e7c75ad15351ad36f8e034
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-19 16:28:10 +0200

    included port 3000 into cors

--- Backend/Node/index.js
+++ Backend/Node/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth-routes.js";
 import cartRoutes from "./routes/cart-routes.js";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
@@ -12,6 +13,7 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri).then(console.log("Connected"));
 
+app.use(cors("http://localhost:3000"))
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
