aa8a7c4722769c951e19eb646369cb5f51a42043
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-20 14:41:48 +0200

    Changed the error messaging

--- Backend/Node/routes/auth-routes.js
+++ Backend/Node/routes/auth-routes.js
@@ -31,31 +31,44 @@ router.post("/register", async (req, res) => {
       res.status(200).json(savedUser);
     } catch (err) {
       res
-        .status(500)
+        .status(400)
         .json({ message: "Something went wrong with the registration." });
     }
   }
 });
 
 router.post("/login", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const foundUser = await User.findOne({email: email});
+  const foundUser = await User.findOne({ email: email });
 
-    if(foundUser){
-        const validPassword = await bcrypt.compare(password, foundUser.password);
-        if(validPassword){
-          const accessToken = jwt.sign(foundUser.toJSON(), process.env.ACCESS_TOKEN);
-          res.status(200).json({user: {firstname: foundUser.firstname, lastname: foundUser.lastname, email: foundUser.email}, accessToken: accessToken});
-        }
-        else{
-            res.status(400).json({error: "Invalid password."});
-        }
+  if (foundUser) {
+    const validPassword = await bcrypt.compare(password, foundUser.password);
+    if (validPassword) {
+      const accessToken = jwt.sign(
+        foundUser.toJSON(),
+        process.env.ACCESS_TOKEN
+      );
+      res
+        .status(200)
+        .json({
+          user: {
+            firstname: foundUser.firstname,
+            lastname: foundUser.lastname,
+            email: foundUser.email,
+          },
+          accessToken: accessToken,
+        });
     }
     else{
-        res.status(400).json({error: "No user with that email address was found."});
+      res.status(400).json({error: "Either the email or password is incorrect. Try again!"})
     }
+  } else {
+    res
+      .status(400)
+      .json({ error: "No user with that email address was found." });
+  }
 });
 
 export default router;
