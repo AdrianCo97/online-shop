28256145579a00fc364513b46187cbaff264f995
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-22 16:32:01 +0200

    Added a header div for the title

--- Frontend/src/components/Categories/Logic/RenderArrayContent.js
+++ Frontend/src/components/Categories/Logic/RenderArrayContent.js
@@ -1,20 +1,25 @@
 import { useNavigate } from "react-router-dom";
 
 function RenderSpecificClothes({ array }) {
-
   const navigate = useNavigate();
   const navigateToProduct = (id) => {
     navigate(`/product/${id}`);
-  }
+  };
 
   return (
     <div className="products">
       {array.map((array) => {
         return (
-          <div className="product" key={array.id} onClick={() => navigateToProduct(array.id)}>
+          <div
+            className="product"
+            key={array.id}
+            onClick={() => navigateToProduct(array.id)}
+          >
+            <div className="product-header">
+              <p className="title">{array.title}</p>
+            </div>
             <img className="thumbnail" src={array.image}></img>
             <div className="product-info">
-              <p className="title">{array.title}</p>
               <p className="price">{array.price} €</p>
             </div>
           </div>
