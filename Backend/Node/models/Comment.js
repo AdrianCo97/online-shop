2ebf085af06ea1506664fc0b2e955297140f6e99
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-24 13:45:10 +0200

    Changed a field in the CommentSchema

--- Backend/Node/models/Comment.js
+++ Backend/Node/models/Comment.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-    productId: Number,
-    email: String,
-    firstname: String,
-    comment: String
-})
+  productId: Number,
+  byUser: { email: String, firstname: String },
+  comment: String,
+});
 
 export default mongoose.Schema("Comment", commentSchema);
