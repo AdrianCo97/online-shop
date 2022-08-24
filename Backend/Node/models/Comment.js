c7c03f8f0e8fc9e8e7f33f45190de7add2f5e759
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-24 14:23:39 +0200

    Changed the schema

--- Backend/Node/models/Comment.js
+++ Backend/Node/models/Comment.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema({
   productId: Number,
   byUser: { email: String, firstname: String },
-  comments: [{
-    comment: {String, required: true}
-  }],
+  comment: {type: String, required: true}
 });
 
 export default mongoose.model("Comment", commentSchema);
