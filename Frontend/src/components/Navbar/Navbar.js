e1c87d0cbba70ca1037fac92e79e7a04e4301a79
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-08-30 14:18:10 +0200

    Created an expandable menu

--- Frontend/src/components/Navbar/Navbar.js
+++ Frontend/src/components/Navbar/Navbar.js
@@ -1,19 +1,32 @@
 import "./navbar.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
 import { CartContext } from "../../contexts/CartContext.js";
 import { UserContext } from "../../contexts/UserContext.js";
 import Badge from "@mui/material/Badge";
+import { Menu, MenuItem } from "@mui/material";
 
 function Navbar() {
   const { productsInCart } = useContext(CartContext);
   const { user } = useContext(UserContext);
+  const [menu, setMenu] = useState({
+    anchorElement: null,
+    open: false,
+  });
 
   const navigate = useNavigate();
   const navigateToShoppingCart = () => {
     navigate("/shoppingcart");
+  };
+
+  const openMenu = (e) => {
+    setMenu({ open: true, anchorElement: e.currentTarget });
+  };
+
+  const closeMenu = () => {
+    setMenu({ open: false, anchorElement: null });
   };
 
   return (
@@ -33,13 +46,25 @@ function Navbar() {
       <div className="account-category">
         <div className="account-section">
           {user.isLoggedIn ? (
-            <Link className="link" to="/profile">{user.user.firstname}</Link>
+            <p className="link" onClick={openMenu}>
+              {user.user.firstname}
+            </p>
           ) : (
             <Link className="link" to="/login">
               <PersonIcon sx={{ verticalAlign: "middle" }} />
               Login
             </Link>
           )}
+          <Menu
+            anchorEl={menu.anchorElement}
+            open={menu.open}
+            anchorOrigin={{ horizontal: "center", vertical: "bottom"}}
+            transformOrigin={{ horizontal: "center", vertical: "top"}}
+            onClose={closeMenu}
+          >
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>Logout</MenuItem>
+          </Menu>
         </div>
         <div className="cartSection">
           <Badge badgeContent={productsInCart.length} color="success">
